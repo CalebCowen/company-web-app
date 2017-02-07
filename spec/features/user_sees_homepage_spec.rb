28fcfffd0326888da13ee04f52c0b7f786dfd655
Caleb <calebcowen@gmail.com>
2017-02-06 22:03:48 -0700

    add description

--- spec/features/user_sees_homepage_spec.rb
+++ spec/features/user_sees_homepage_spec.rb
@@ -1,12 +1,19 @@
-require 'rails_helper'
-
-RSpec.feature 'user sees homepage' do
-  describe 'homepage' do
-    scenario 'user visits root path' do
-      
-      visit root_path
-
-      expect(page).to have_content 'Dapper Codes'
-    end
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.feature 'user sees homepage' do
+#   describe 'homepage' do
+#     scenario 'user visits root path and sees content' do
+#       create(:project)
+#       Project.create(name: 'Second Project', description: 'Another Project', url: 'example2.com')
+#       visit root_path
+#
+#       expect(page).to have_content 'Dapper Codes'
+#       expect(page).to have_content 'Project'
+#       expect(page).to have_content 'Second Project'
+#       expect(page).to have_content 'This is a project'
+#       expect(page).to have_content 'Another Project'
+#       expect(page).to have_link 'www.example.com'
+#       expect(page).to have_link 'example2.com'
+#     end
+#   end
+# end
