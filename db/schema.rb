e7f8777eee04ff21a12674d601068ce4ecfd3838
Caleb <calebcowen@gmail.com>
2017-02-22 13:43:15 -0700

    Add status migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221062701) do
+ActiveRecord::Schema.define(version: 20170222203345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.text   "description"
-    t.string "url"
+    t.string  "name"
+    t.text    "description"
+    t.string  "url"
+    t.integer "status",      default: 0
   end
 
 end
