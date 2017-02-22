6b2133fa232f0093871228f518d273bdc89d2151
Caleb <calebcowen@gmail.com>
2017-02-22 15:19:46 -0700

    Add payment link and form on payment page and test

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,10 @@
+class PaymentsController < ApplicationController
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  
+
   root 'home#index'
+
+  resources :payments, only: [:new, :create]
 end
